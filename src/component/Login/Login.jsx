d30b9ab7129f81211f4b5a7a0b337b1b5d41161f
reza <69678342+rezac78@users.noreply.github.com>
2021-07-24 01:44:43 -0700

    oky

--- src/component/Login/Login.jsx
+++ src/component/Login/Login.jsx
@@ -1,11 +1,53 @@
-import React,{useState} from 'react';
+import React, { useState, useRef } from 'react';
+import { loginUser } from '../../services/userServices';
+import SimpleReactValidator from 'simple-react-validator';
+import { withRouter } from "react-router-dom";
+import { toast } from 'react-toastify';
 
-const Login = () => {
-    const [email,setEmail]=useState("")
-    const [password,setPassword]=useState("")
+const Login = ({ history }) => {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [, forceUpdate] = useState();
 
-    const handleSubmit=event=>{
+    const validaton = useRef(new SimpleReactValidator({
+        messages: {
+            required: "پرکردن این فیلد الزامی است",
+            min: "کمتر از 5 کاراکتر نباید باشد",
+            email: "ایمیل نوشته شده صحیح نمی باشد"
+        },
+        element: message => <div style={{ color: "red" }}>{message}</div>
+    }));
+
+    const reset = () => {
+        setEmail("");
+        setPassword("");
+    }
+
+    const handleSubmit = async event => {
         event.preventDefault();
+
+        const user = {
+            email,
+            password
+        }
+        try {
+            if (validaton.current.allValid()) {
+                const { status, data } = await loginUser(user)
+                if (status === 201) {
+                    toast.success("ورود با موفقیت ساخته شد", { position: "top-right", closeOnClick: true })
+                    localStorage.setItem("token", data.token)
+                    history.replace('/');
+                    reset();
+                }
+            } else {
+                validaton.current.showMessage();
+                forceUpdate(1);
+            }
+
+        } catch (err) {
+            toast.error("مشکلی پیش اومده", { position: "top-right", closeOnClick: true })
+            console.log(err)
+        }
     }
     return (
         <main className="form-signin">
@@ -14,11 +56,21 @@ const Login = () => {
 
                 <div className="form-floating">
                     <label for="floatingInput">Email address</label>
-                    <input name="email" type="email" className="form-control" id="floatingInput" placeholder="name@example.com" value={email} onChange={(e)=>setEmail(e.target.value)} />
+                    <input name="email" type="email" className="form-control" id="floatingInput" placeholder="name@example.com" value={email} onChange={(e) => {
+                        setEmail(e.target.value)
+                        validaton.current.showMessageFor("email")
+                    }
+                    } />
+                    {validaton.current.message("email", email, "required|email")}
                 </div>
                 <div className="form-floating">
                     <label for="floatingPassword">Password</label>
-                    <input name="password" type="password" className="form-control" id="floatingPassword" placeholder="Password" value={password} onChange={(e)=>setPassword(e.target.value)} />
+                    <input name="password" type="password" className="form-control" id="floatingPassword" placeholder="Password" value={password} onChange={(e) => {
+                        setPassword(e.target.value)
+                        validaton.current.showMessageFor("password")
+                    }
+                    } />
+                    {validaton.current.message("password", password, "required|min:5")}
                 </div>
 
                 <div className="checkbox mb-3">
@@ -35,4 +87,4 @@ const Login = () => {
     )
 }
 
-export default Login;
+export default withRouter(Login);
--- src/component/Register/Register.jsx
+++ src/component/Register/Register.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useRef, useState } from 'react';
 import { toast } from 'react-toastify';
+import SimpleReactValidator from 'simple-react-validator';
 import { registerUser } from './../../services/userServices';
 
 
@@ -8,6 +8,17 @@ const Register = () => {
     const [fullname, setFullname] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [policy, setPolicy] = useState();
+    const [, forceUpdate] = useState();
+    const validaton = useRef(new SimpleReactValidator({
+        messages: {
+            required: "پرکردن این فیلد الزامی است",
+            min: "کمتر از 5 کاراکتر نباید باشد",
+            email: "ایمیل نوشته شده صحیح نمی باشد"
+        },
+        element: message => <div style={{ color: "red" }}>{message}</div>
+    }));
+
 
     const reset = () => {
         setEmail("");
@@ -23,20 +34,20 @@ const Register = () => {
             password
         }
         try {
-            const { status } = await registerUser(user)
-            if (status === 201) {
-                toast.success("کاربر با موفقیت ساخته شد", { position: "top-right", closeOnClick: true })
-                reset();
+            if (validaton.current.allValid()) {
+                const { status } = await registerUser(user)
+                if (status === 201) {
+                    toast.success("کاربر با موفقیت ساخته شد", { position: "top-right", closeOnClick: true })
+                    reset();
+                }
+            } else {
+                validaton.current.showMessage();
+                forceUpdate(1);
             }
         } catch (err) {
             toast.error("مشکلی پیش اومده", { position: "top-right", closeOnClick: true })
             console.log(err)
         }
-
-
-
-
-
         console.log(user);
     }
     return (
@@ -46,22 +57,39 @@ const Register = () => {
 
                 <div className="form-floating">
                     <label for="floatingInput">Email address</label>
-                    <input name="fullname" type="text" className="form-control" id="floatingInput" placeholder="fullname" value={fullname} onChange={e => setFullname(e.target.value)} />
+                    <input name="fullname" type="text" className="form-control" id="floatingInput" placeholder="fullname" value={fullname} onChange={e => {
+                        setFullname(e.target.value);
+                        validaton.current.showMessageFor("fullname")
+                    }} />
+                    {validaton.current.message("fullname", fullname, "required|min:4")}
                 </div>
 
                 <div className="form-floating">
                     <label for="floatingInput">Email address</label>
-                    <input name="email" type="email" className="form-control" id="floatingInput" placeholder="name@example.com" value={email} onChange={e => setEmail(e.target.value)} />
+                    <input name="email" type="email" className="form-control" id="floatingInput" placeholder="name@example.com" value={email} onChange={e => {
+                        setEmail(e.target.value)
+                        validaton.current.showMessageFor("email");
+                    }} />
+                    {validaton.current.message("email", email, "required|email")}
                 </div>
                 <div className="form-floating">
                     <label for="floatingPassword">Password</label>
-                    <input name="password" type="password" className="form-control" id="floatingPassword" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
+                    <input name="password" type="password" className="form-control" id="floatingPassword" placeholder="Password" value={password} onChange={e => {
+                        setPassword(e.target.value)
+                        validaton.current.showMessageFor("password")
+                    }
+                    } />
+                    {validaton.current.message("password", password, "required|min:5")}
                 </div>
 
                 <div className="checkbox mb-3">
                     <label>
-                        <input name="remember" type="checkbox" value="remember-me" /> Remember me
+                        <input name="policy" type="checkbox" value={policy} onChange={(e)=>{
+                            setPolicy(e.currentTarget.checked)
+                            validaton.current.showMessageFor("policy")
+                        }} /> I accept the rules
                     </label>
+                    {validaton.current.message("policy",policy,"required")}
                 </div>
                 <div className="form-group">
                     <div className="g-recaptcha" data-sitekey="6Ld8NV0bAAAAAG6of_pki3Rc288CIp7Gl1rAYBYg"></div>
